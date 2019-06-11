c2baad7962c63ec0d20873d2744f634474b63f42
cieslaluk <ciesla.luk@gmail.com>
2019-06-11 11:39:42 +0200

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
+function navigationBarResponsive() {
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
-}
+};
 
-var $link = $(".topnav li:not(:last-child)");
+let $link = $(".topnav li:not(:last-child)");
 $link.click(function () {
     if ($("#myTopnav").hasClass("responsive")) {
         $("#myTopnav").removeClass("responsive");
@@ -20,4 +20,19 @@ $(window).scroll(function () {
     } else {
         $("nav").removeClass("scrolling");
     }
+});
+
+$(document).ready(function () {
+
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
 });
