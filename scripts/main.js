6c5261c84740bafc8d77220c999d0f361c8fd1fb
cieslaluk <ciesla.luk@gmail.com>
2019-06-13 11:30:49 +0200

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,8 @@
-function navigationBarResponsive() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-};
-
-let $link = $(".topnav li:not(:last-child)");
-$link.click(function () {
-    if ($("#myTopnav").hasClass("responsive")) {
-        $("#myTopnav").removeClass("responsive");
-    }
-});
-
-$(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
-        $("nav").addClass("scrolling");
-    } else {
-        $("nav").removeClass("scrolling");
-    }
-});
+$(document).ready(function(){
+    $('.menu').click(function(){
+        $('ul').toggleClass('active');
+    })
+})
 
 $(document).ready(function () {
 
